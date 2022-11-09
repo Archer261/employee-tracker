395e5216b2b6a42088893c98b5ef7233adfce9ba
Joseph Archer <viewtiful.kja@gmail.com>
2022-11-09 18:50:28 -0500

    terminal ins now working

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-const cTable = require('console.table');
-
+const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require("inquirer");
+// const db = require('./db/connection');
 
-async function main() {
-    // get the client
-    const mysql = require('mysql2/promise');
-    // create the connection
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'Mokuba983!!',
-        port: 3001,
-        database: 'employee_tracker'
-    });
-    const [rows, fields] = await main.execute('SELECT * FROM employee');
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-    console.table(
-        console.log(`
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// create the connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Mokuba983!!',
+    port: 3001,
+    database: 'employee_tracker'
+});
+
+console.log(`
         ███████╗███╗░░░███╗██████╗░██╗░░░░░░█████╗░██╗░░░██╗███████╗███████╗
         ██╔════╝████╗░████║██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝██╔════╝██╔════╝
         █████╗░░██╔████╔██║██████╔╝██║░░░░░██║░░██║░╚████╔╝░█████╗░░█████╗░░
@@ -30,30 +33,98 @@ async function main() {
         ░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
         ░░░██║░░░██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║
         ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝
-    `),
-        console.log(main()),
+    `)
 
+main();
 
-        [
-            // Menu Header
-            {
-                name: 'foo',
-                age: 10
-            }, {
-                name: 'bar',
-                age: 20
-            }
-        ]);
-
-
+function main() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Main Menu",
+            name: "choice",
+            choices: [
+                "Add New Employee",
+                "Delete Employee",
+                "View All Employees",
+                "Add New Role",
+                "Delete Role",
+                "View All Roles",
+                "Add New Department",
+                "Delete Department",
+                "View All Departements",
+                "Exit"
+            ],
+        },
+    ]).then((choices) => {
+        switch (choices) {
+            case "Add New Employee":
+                addNewEmp();
+                break;
+            case "Delete Employee":
+                deleteEmp();
+                break;
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+            case "Add New Role":
+                addNewRole();
+                break;
+            case "Delete Role":
+                deleteRole();
+                break;
+            case "View All Roles":
+                viewAllRoles();
+                break;
+            case "Add New Department":
+                addNewDept();
+                break;
+            case "Delete Department":
+                deleteDept();
+                break;
+            case "View All Departements":
+                viewAllDept();
+                break;
+            case "Exit":
+                break;
+            default:
+                main()
+        }
+    })
 }
 
+function addNewEmp() {
+    return;
+}
 
-
-
+function deleteEmp() {
+    return;
+}
 
 function viewAllEmployees() {
     return;
 }
+
+function addNewRole() {
+    return;
+}
+
+function deleteRole() {
+    return;
+}
+
+function viewAllRoles() {
+    return;
+}
+function addNewDept() {
+    return;
+}
+function deleteDept() {
+    return;
+}
+function viewAllDept() {
+    return;
+}
+
 
 
